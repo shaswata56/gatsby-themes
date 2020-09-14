4ca34120910bf9479d569855762f78db64b4272f
Shaswata Das <shaswata56@gmail.com>
2020-09-14 07:00:26 +0600

    Update cara.tsx

--- themes/gatsby-theme-cara/src/templates/cara.tsx
+++ themes/gatsby-theme-cara/src/templates/cara.tsx
@@ -10,8 +10,8 @@ const Cara = () => (
   <Layout>
     <Parallax pages={5}>
       <Hero offset={0} factor={1} />
-      <Projects offset={1} factor={2} />
       <About offset={3} factor={1} />
+      <Projects offset={1} factor={2} />
       <Contact offset={4} factor={1} />
     </Parallax>
   </Layout>
